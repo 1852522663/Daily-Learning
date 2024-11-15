3dd86122192509ffc3715f41433e33ae9e0f1bcb
yuehewei <yuehewei@kuaishou.com>
2024-11-15 17:11:08 +0800

    feat

---
+++ learning-demo-api/src/main/java/com/learning/demo/constant/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.learning.demo.constant;
+
+public final class ApplicationConstants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_TEST = "test";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+}
---
+++ learning-demo-api/src/main/java/com/learning/demo/plugin/ApplicationWebServerCustomizer.java
@@ -0,0 +1,33 @@
+package com.learning.demo.plugin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.core.Ordered;
+
+/**
+ * 定制 Embedded WebServer，比参数化配置更加灵活，建议统一在这里设置
+ *
+ * Write the code. Change the world.
+ *
+ * @author trang
+ */
+public class ApplicationWebServerCustomizer
+        implements WebServerFactoryCustomizer<ConfigurableWebServerFactory>, Ordered {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationWebServerCustomizer.class);
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+    }
+
+    /**
+     * 在 SpringBoot 内置的 {@link WebServerFactoryCustomizer} 之后执行
+     */
+    @Override
+    public int getOrder() {
+        return 100;
+    }
+
+}
